5e745e18dd792c8d2ff01560f7f74c8526cf00e2
Sam Keyser <samkeyser1@gmail.com>
2024-02-02 16:58:40 -0600

    enhance app

--- application.py
+++ application.py
@@ -1,14 +1,50 @@
 from flask import Flask, render_template
+from NamedAtomicLock import NamedAtomicLock
 from fortune import fortune
+from nms import frameinate
 
 import cowsay
 
 application = app = Flask(__name__)
+frames_lock = NamedAtomicLock('wsgi-lock')
+
+frames = []
+frame_ptr = 0
+@application.route('/frames')
+def get_frame() -> str:
+    global frames
+    global frame_ptr
+
+    frame = ''
+    status = -1
+    if frames_lock.acquire():
+        print("frames (inside get_frame)=", len(frames))
+        if len(frames) == 0:
+            status = 2
+        elif frame_ptr < len(frames):
+            frame = frames[frame_ptr]
+            frame_ptr += 1
+        elif frame_ptr == len(frames):
+            frame = frames[-1]
+            status = 1
+        print('get', frame_ptr, len(frames), status)
+
+        frames_lock.release()
+    return {'frame': frame, 'status': status}
 
 @application.route('/')
-def get_fortune() -> str:
-    ret = cowsay.get_output_string('cow', fortune())
-    return render_template('index.html', user_fortune=ret)
+def load() -> str:
+    global frames
+    global frame_ptr
+
+    if frames_lock.acquire():
+        text = cowsay.get_output_string('cow', fortune())
+        frames = frameinate(text)
+        frame_ptr = 0
+
+        print('frames =', len(frames))
+        frames_lock.release()
+    return render_template('index.html')
 
 if __name__ == "__main__":
     application.run()
